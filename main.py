f02f97d93e7edf2cb99153e88268a81bbe5c5f76
dhruv-0512 <dhruvh3012@gmail.com>
2025-07-18 23:30:46 +0530

    Game created successfully

--- main.py
+++ main.py
@@ -1,47 +1,105 @@
-board=[[' ',' ',' ',],[' ',' ',' ',],[' ',' ',' ',]]
-def display():
-    print(game_list[0][0],game_list[0][1],game_list[0][2])
-    print(game_list[1][0],game_list[1][1],game_list[1][2])
-    print(game_list[2][0],game_list[2][1],game_list[2][2])
+def game():
+    board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    a1 = ''
+    a2 = ''
 
-display()
-def selectXorO():
-    a='wrong'
-    while a not in ['X','O']:
-        a=input("Player one X or O:")
-        if a not in ['X','O']:
-            print("Select either X or O only")
-    if a=='X':
-        a1='X'
-        print(f"Player-1 has opted {a} Player-2 gets O")
-    else:
-        a2='O'
-        print(f"Player-1 has opted {a} Player-2 gets X")
-selectXorO()       
-def getanswerplayer1():
-    print("Player-1 your chance:")
-    print("Enter the position u want to enter the row and column")
-    rpos1='wrong'
-    cpos1='wrong'
-    while rpos1 and cpos1 not in [0,1,2]:
-        rpos1=int(input())
-        cpos1=int(input())
-        if rpos1 and cpos1 not in [0,1,2]:
-            print("Please enter valid literals")
-    board[rpos1][cpos1]=a1
-def getanswerplayer2():
-    print("Player-1 your chance:")
-    print("Enter the position u want to enter the row and column")
-    rpos2='wrong'
-    cpos2='wrong'
-    while rpos2 and cpos2 not in [0,1,2]:
-        rpos2=int(input())
-        cpos2=int(input())
-        if rpos2 and cpos2 not in [0,1,2]:
-            print("Please enter valid literals")
-    board[rpos2][cpos2]=a2
+    def display():
+        print("\n   0   1   2")
+        for i in range(3):
+            print(i, ' | '.join(board[i]))
+            if i < 2:
+                print("  -----------")
+        print()
 
-def checkwin():
-    
-        
-    
+    def selectXorO():
+        nonlocal_a1 = ''
+        nonlocal_a2 = ''
+        a = 'wrong'
+        while a not in ['X', 'O']:
+            a = input("Player 1, choose X or O: ").upper()
+            if a not in ['X', 'O']:
+                print("Invalid choice. Please choose X or O.")
+        if a == 'X':
+            nonlocal_a1, nonlocal_a2 = 'X', 'O'
+        else:
+            nonlocal_a1, nonlocal_a2 = 'O', 'X'
+        print(f"Player 1 is '{nonlocal_a1}', Player 2 is '{nonlocal_a2}'")
+        return nonlocal_a1, nonlocal_a2
+
+    def get_move_player1():
+        while True:
+            try:
+                print(f"Player 1's turn ({a1})")
+                r = int(input("Enter row (0-2): "))
+                c = int(input("Enter column (0-2): "))
+                if r not in [0,1,2] or c not in [0,1,2]:
+                    print("Invalid position.")
+                    continue
+                if board[r][c] != ' ':
+                    print("Spot already taken.")
+                    continue
+                board[r][c] = a1
+                break
+            except:
+                print("Enter valid numbers.")
+
+    def get_move_player2():
+        while True:
+            try:
+                print(f"Player 2's turn ({a2})")
+                r = int(input("Enter row (0-2): "))
+                c = int(input("Enter column (0-2): "))
+                if r not in [0,1,2] or c not in [0,1,2]:
+                    print("Invalid position.")
+                    continue
+                if board[r][c] != ' ':
+                    print("Spot already taken.")
+                    continue
+                board[r][c] = a2
+                break
+            except:
+                print("Enter valid numbers.")
+
+    def check_winner():
+        for i in range(3):
+            if board[i][0] == board[i][1] == board[i][2] != ' ':
+                return board[i][0]
+            if board[0][i] == board[1][i] == board[2][i] != ' ':
+                return board[0][i]
+        if board[0][0] == board[1][1] == board[2][2] != ' ':
+            return board[0][0]
+        if board[0][2] == board[1][1] == board[2][0] != ' ':
+            return board[0][2]
+        for row in board:
+            if ' ' in row:
+                return None
+        return 'Draw'
+
+    a1, a2 = selectXorO()
+    display()
+
+    while True:
+        get_move_player1()
+        display()
+        result = check_winner()
+        if result:
+            if result == 'Draw':
+                print("It's a draw!")
+            elif result == a1:
+                print("Player 1 wins!")
+            else:
+                print("Player 2 wins!")
+            break
+
+        get_move_player2()
+        display()
+        result = check_winner()
+        if result:
+            if result == 'Draw':
+                print("It's a draw!")
+            elif result == a1:
+                print("Player 1 wins!")
+            else:
+                print("Player 2 wins!")
+            break
+game()
